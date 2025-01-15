72ed6a6d43203f4dd73ce164973aac3f0e0f5699
gagansarroye8 <gagandeep.singh21@edu.sait.ca>
2025-01-15 13:14:24 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * CPRG 211 D lab 0
+ * Author: Jolanta
+ * When: Winter 2025
+ */
+
+using Lab0;
+
+int low, high;
+int diff; // difference between low and high
+// get positive low number
+low = Utilities.GetPositiveInt("low number");
+//Console.WriteLine($"Low number is {low}");
+
+// get high number that is greater than low
+high = Utilities.GetIntInRange("high number", low + 1, Int32.MaxValue);
+//Console.WriteLine($"High number is {high}");
+
+// calculate and print the difference
+diff = high - low;
+Console.WriteLine($"The difference between {low} and {high} is {diff}");
+
+// create an array to hold numbers from low to high
+int[] numbers = new int[diff + 1];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = low + i;
+}
+
+    Console.WriteLine("Numbers in the array:");
+for(int i=0;  i<numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+// create a new file "numbers.txt" and write to it the numbers from the array in reverse order
+StreamWriter streamWriter = File.CreateText("numbers.txt"); // in the same directory as .exe file
+for(int i = numbers.Length - 1; i >= 0; i--)
+{
+    streamWriter.WriteLine(numbers[i]);
+}
+streamWriter.Close(); // important to finalize the writing
+
+// read the numbers back from the file into the same array and display
+StreamReader streamReader = File.OpenText("numbers.txt");
+string nextLine;
+for (int i = 0; i < numbers.Length; i++)
+{
+    nextLine = streamReader.ReadLine()??"";
+    numbers[i] = Convert.ToInt32(nextLine);
+}
+streamReader.Close();
+
+Console.WriteLine("Numbers from the file:");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
---
+++ Utilities.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    /// <summary>
+    /// A repository of methods for reading and validating data and useful calculations
+    /// </summary>
+    internal static class Utilities
+    {
+        /// <summary>
+        /// read and validate positive decimal value
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <returns>positive decimal</returns>
+        public static decimal GetPositiveDecimal(string prompt)
+        {
+            bool gotIt = false;
+            decimal result = 0;
+
+            while (!gotIt) // while gotIt is false
+            {
+                Console.WriteLine($"Enter {prompt}");
+                string input = Console.ReadLine() ?? "";
+                if (Decimal.TryParse(input, out result))
+                {
+                    if(result > 0)
+                    {
+                        gotIt = true; // success
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {prompt} must be a number");
+                } 
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// read and validate positive int value
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <returns>positive int</returns>
+        public static int GetPositiveInt(string prompt)
+        {
+            bool gotIt = false;
+            int result = 0;
+
+            while (!gotIt) // while gotIt is false
+            {
+                Console.WriteLine($"Enter {prompt}");
+                string input = Console.ReadLine() ?? "";
+                if (Int32.TryParse(input, out result))
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true; // success
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// reads and validates int value within given range
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <param name="minValue">minimum value for the range</param>
+        /// <param name="maxValue">maximum value for the range</param>
+        /// <returns></returns>
+        public static int GetIntInRange(string prompt, int minValue, int maxValue)
+        {
+
+            bool gotIt = false;
+            int result = 0;
+
+            while (!gotIt) // while gotIt is false
+            {
+                Console.WriteLine($"Enter {prompt}");
+                string input = Console.ReadLine() ?? "";
+                if (Int32.TryParse(input, out result))
+                {
+                    if (result >= minValue && result <= maxValue)
+                    {
+                        gotIt = true; // success
+                    }
+                    else
+                    {
+                        if(minValue == Int32.MinValue)
+                            Console.WriteLine($"Error: {prompt} must be less than or equal to {maxValue}");
+                        else if(maxValue == Int32.MaxValue)
+                            Console.WriteLine($"Error: {prompt} must be greater than or equal to {minValue}");
+                        else
+                            Console.WriteLine($"Error: {prompt} must be between {minValue} and {maxValue}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+            return result;
+        }
+
+
+    } // class
+} // namespace
